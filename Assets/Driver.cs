77b429fc8912327006cef91085c60cbc0004a2da
Andrew Huntington <andrew.huntington@gmail.com>
2022-12-31 20:40:12 +0900

    Add speed boost and slow down functionality

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-  [SerializeField] float steerSpeed = 0.1f;
-  [SerializeField] float moveSpeed = 0.01f;
+  [SerializeField] float steerSpeed = 300f;
+  [SerializeField] float moveSpeed = 15f;
+  [SerializeField] float slowSpeed = 10f;
+  [SerializeField] float boostSpeed = 30f;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-  }
-
-  // Update is called once per frame
   void Update()
   {
     float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
     float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
     transform.Rotate(0, 0, -steerAmount);
     transform.Translate(0, moveAmount, 0);
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Booster")
+    {
+      moveSpeed = boostSpeed;
+    }
+  }
+
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    moveSpeed = slowSpeed;
   }
 }
